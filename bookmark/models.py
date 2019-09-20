b6211b832898aee71693fcf1827ccf628a06abff
unknown <ageha0@naver.com>
2019-09-20 11:36:17 +0900

    manage.py edit 9

--- bookmark/models.py
+++ bookmark/models.py
@@ -7,7 +7,7 @@ class Bookmark(models.Model):
     site_name = models.CharField(max_length=100)
     url = models.URLField('Site URL')
     def __str__(self):
-        return "이름 : "+self.site_name + ", 주소 : " + self.url
+        return "name : "+self.site_name + ", address : " + self.url
 
     def get_absolute_url(self):
         return reverse('detail', args=[str(self.id)])
