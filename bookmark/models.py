1df0ee15cb7151702f03eb596b36bb798191799d
unknown <ageha0@naver.com>
2019-09-20 11:34:53 +0900

    manage.py edit 7

--- bookmark/models.py
+++ bookmark/models.py
@@ -7,7 +7,6 @@ class Bookmark(models.Model):
     site_name = models.CharField(max_length=100)
     url = models.URLField('Site URL')
     def __str__(self):
-        #객체를 출력할때 나타날 값
         return "이름 : "+self.site_name + ", 주소 : " + self.url
 
     def get_absolute_url(self):
